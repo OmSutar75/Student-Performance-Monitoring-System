695852e45f30e8faba48e7875433db78c26a4227
HackerOm <hackeromsutar@gmail.com>
2025-12-20 15:49:25 +0530

    Adding Coruse And Course Group class

---
+++ SPT/StudentManagment/StudentPerformanceManagment/Models/Staff.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPerformanceManagement.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        public string? Mobile { get; set; }
+
+        public ICollection<Tasks> Tasks { get; set; } = new List<Tasks>();
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+    }
+}
