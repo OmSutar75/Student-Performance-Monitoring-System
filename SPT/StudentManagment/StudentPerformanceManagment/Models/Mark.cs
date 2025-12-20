7b2c850f353ff2c0e4d7006348e53ce5e500173b
Swarupanand05 <spb05092004@gmail.com>
2025-12-20 15:24:40 +0530

    Mark class Added

---
+++ SPT/StudentManagment/StudentPerformanceManagment/Models/Mark.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPerformanceManagment.Models
+{
+    public class Mark
+    {
+        [Key]
+        public int MarkId { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; } = null!;
+
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; } = null!;
+
+        public int TheoryMarks { get; set; }
+        public int LabMarks { get; set; }
+        public int InternalMarks { get; set; }
+    }
+}
