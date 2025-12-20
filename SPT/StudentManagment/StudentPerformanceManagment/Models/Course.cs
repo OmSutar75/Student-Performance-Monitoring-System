f0478e92bba5af3587ce6d1fdf808720deeee482
HackerOm <hackeromsutar@gmail.com>
2025-12-20 15:10:58 +0530

    Added Course And Course Group Model Class

---
+++ SPT/StudentManagment/StudentPerformanceManagment/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPerformanceManagment.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CourseName { get; set; } = null!;
+
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        [MaxLength(50)]
+        public string? Duration { get; set; }
+
+        public decimal Fees { get; set; }
+
+        public ICollection<Subject> Subjects { get; set; } = new List<Subject>();
+        public ICollection<CourseGroup> CourseGroups { get; set; } = new List<CourseGroup>();
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
