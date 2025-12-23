7954584816235578fe3fe13a07121e3d25fd21c7
Anurag Londhe <anuraglondhe9052.com>
2025-12-23 23:21:32 +0530

    Student Controller added

---
+++ SPT/StudentManagment/StudentPerformanceManagment/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPerformanceManagment.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+    }
+}
