b1330dd8234cbb9431c91265e34714bb28840ff5
HackerOm <hackeromsutar@gmail.com>
2025-12-23 12:55:04 +0530

    Created Basic Staff Add Marks functionality in project

---
+++ SPT/StudentManagment/StudentPerformanceManagment/Controllers/StaffController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentPerformanceManagement.Models;
+using StudentPerformanceManagment.Models;
+using StudentPerformanceManagment.Models.ViewModel;
+
+namespace StudentPerformanceManagment.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public StaffController(UserManager<AppUser> userManager,
+        ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult AddMark()
+       {
+            //var model= _context.Students.Select(s => new MarkViewModel { Prn = s.PRN, Name = s.Name });
+
+            var model = new MarkViewModel
+            {
+                students = _context.Students.ToList(),
+
+            };
+
+              
+            return View(model);
+        }
+    }
+}
---
+++ SPT/StudentManagment/StudentPerformanceManagment/Models/ViewModel/MarkViewModel.cs
@@ -0,0 +1,24 @@
+﻿using StudentPerformanceManagement.Models;
+using System.ComponentModel;
+
+namespace StudentPerformanceManagment.Models.ViewModel
+{
+    public class MarkViewModel
+    {
+
+        /*public string Prn {  get; set; }
+        public string Name { get; set; }*/
+
+        public List<Student> students { get; set; }
+
+        public int TheoryMarks { get; set; }
+        public int LabMarks { get; set; }
+        public int InternalMarks { get; set; }
+
+        public int Total {  get; set; }
+
+        public string Status { get; set; } 
+
+
+    }
+}
