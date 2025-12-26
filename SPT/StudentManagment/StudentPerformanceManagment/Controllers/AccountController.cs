d76ab170f51a2d1ffd356b57cdc2dfaab74a1799
Swarupanand05 <spb05092004@gmail.com>
2025-12-26 08:55:01 +0530

    Conflict remove

--- SPT/StudentManagment/StudentPerformanceManagment/Controllers/AccountController.cs
+++ SPT/StudentManagment/StudentPerformanceManagment/Controllers/AccountController.cs
@@ -50,19 +50,14 @@ namespace IdentityDemo.Controllers
         {
             var user = await _userManager.GetUserAsync(User);
             if (await _userManager.IsInRoleAsync(user, "Admin"))
-<<<<<<< HEAD
+
                 return RedirectToAction("Dashboard", "Admin"); ;
 
             if (await _userManager.IsInRoleAsync(user, "Staff"))
                 return RedirectToAction("Dashboard", "Staff");
-=======
-                            return RedirectToAction("Dashboard", "Admin"); 
 
-            if (await _userManager.IsInRoleAsync(user, "Staff"))
-                            return RedirectToAction("Dashboard","Staff");
->>>>>>> ModelCreationMark
+               return RedirectToAction("Dashboard", "Student"); 
 
-            return RedirectToAction("Dashboard", "Student");
 
 
         }
@@ -73,12 +68,12 @@ namespace IdentityDemo.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Login");
         }
-<<<<<<< HEAD
+
     }
 }
 
 
 
-=======
-    }
->>>>>>> ModelCreationMark
+
+    
+
