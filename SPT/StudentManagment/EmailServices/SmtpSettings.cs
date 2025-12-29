bb4817a4ee9cbee1f35f051f6f15160bbdc6751d
SaklenManjire <saklenmanjire032@gmail.com>
2025-12-29 16:08:59 +0530

    Changes

--- SPT/StudentManagment/EmailServices/SmtpSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EmailService
-{
-    public class SmtpSettings
-    {
-
-    }
-}
--- SPT/StudentManagment/StudentPerformanceManagment/Controllers/StudentController.cs
+++ SPT/StudentManagment/StudentPerformanceManagment/Controllers/StudentController.cs
@@ -209,9 +209,5 @@ namespace StudentPerformanceManagement.Controllers
         }
 
 
-       
-
-
-
     }
 }
