9a4f1fb14ae21bf5c68f882ea3df26f218719020
Anurag Londhe <anuraglondhe9052.com>
2025-12-30 12:49:24 +0530

    Admin portal changes

--- SPT/StudentManagment/StudentPerformanceManagment/Models/ViewModel/StudentEnrollmentViewModel1.cs
+++ SPT/StudentManagment/StudentPerformanceManagment/Models/ViewModel/StudentEnrollmentViewModel1.cs
@@ -5,11 +5,6 @@ namespace StudentPerformanceManagement.ViewModel
 {
     public class StudentEnrollmentViewModel1
     {
-        public string Name { get; set; }
-        public string? Email { get; set; }
-        public string? MobileNo { get; set; }
-
-        public string ProfileImagePath { get; set; } = "profile.img";
-    }
+        }
 }
 
